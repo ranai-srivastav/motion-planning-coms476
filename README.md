74a5ef55ff198a5bc9df707c493991853d28b08e
ranais <ranaisrivastav@gmail.com>
2023-02-17 02:59:11 +0000

    Package

--- README.md
+++
@@ -1,5 +0,0 @@
-# Course repository for COM S 476/576 - Motion Strategy Algorithms and Applications, Spring 2023
-
-* Computer Lab
-* Homework
-* Project
--- catkin_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /workspaces/mp-planning-COMS476/motion-planning-coms476/catkin_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
