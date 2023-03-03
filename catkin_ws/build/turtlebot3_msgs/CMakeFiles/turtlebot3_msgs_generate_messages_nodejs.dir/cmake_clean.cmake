6817ba8b92377723764e2219f5e249e37ce23633
ranais <ranaisrivastav@gmail.com>
2023-03-03 23:27:26 +0000

    changing git ignore

---
+++ catkin_ws/build/turtlebot3_msgs/CMakeFiles/turtlebot3_msgs_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/root/motion-planning-coms476/catkin_ws/devel/share/gennodejs/ros/turtlebot3_msgs/msg/SensorState.js"
+  "/root/motion-planning-coms476/catkin_ws/devel/share/gennodejs/ros/turtlebot3_msgs/msg/Sound.js"
+  "/root/motion-planning-coms476/catkin_ws/devel/share/gennodejs/ros/turtlebot3_msgs/msg/VersionInfo.js"
+  "CMakeFiles/turtlebot3_msgs_generate_messages_nodejs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlebot3_msgs_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
